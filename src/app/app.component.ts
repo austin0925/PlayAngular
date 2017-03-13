8e6ecdd3ebd4c8eedeb3767837d74492ec9bb214
austin-wang <austin-wang@nec.com.tw>
2017-03-13 15:49:58 +0800

    6-routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
+import { HeroService} from './hero.service'
 
 @Component({
   selector: 'my-app',
@@ -68,13 +69,23 @@ import { Hero } from './hero';
   `],
 })
 
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(
+      heroes => this.heroes = heroes
+    );
+  }
+
+  
   title = 'Tour of Heroes'
   hero: Hero = {
     id: 1,
     name: 'windstorm'
   }
-  heroes = HEROES;
+
+  heroes: Hero[];
   selectedHero: Hero;
 
   appName = 'Angular|quickstart'; 
@@ -85,16 +96,3 @@ export class AppComponent  {
 
 
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
