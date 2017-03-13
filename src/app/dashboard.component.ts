0c661c72f9e8f032ae8db0bd3a8481eae1937593
austin-wang <austin-wang@nec.com.tw>
2017-03-13 17:57:27 +0800

    6-routing-p2

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -17,7 +17,7 @@ export class DashboardComponent implements OnInit {
 
     ngOnInit(): void {
         this.heroService.getHeroes().then(
-            heroes => this.heroes = heroes
+            heroes => this.heroes = heroes.slice(1, 5)
         );
     }
 }
